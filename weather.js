e893321d4ebb776bd98c6afeaf140a563870168a
Souradip Mandal <67626543+deep2609@users.noreply.github.com>
2021-03-31 08:06:49 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -17,13 +17,12 @@ app.post("/",function(req,res){
   https.get(url,function(response){
     response.on("data",function(data){
       const weatherData = JSON.parse(data);
-     // console.log(weatherData);
       var temp = weatherData.main.temp;
       var sky = weatherData.weather[0].description;
       var icon = weatherData.weather[0].icon;
 
       const urlImage = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
-     // console.log(sky);
+     
       res.write("<h1>The temperature in "+cityName+" is "+temp+" degrees Celsius.</h1>");
       res.write("<p>Conditions are : "+sky+"</p>");
       res.write("<img src="+urlImage+">");
